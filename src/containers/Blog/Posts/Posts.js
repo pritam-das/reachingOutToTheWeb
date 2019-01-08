e3e732051355293ff2609e26a321c180b1d651cb
PRITAM <pritam.das@mail.utoronto.ca>
2019-01-08 15:11:48 -0500

    Passing parameters with routes

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import { Link } from 'react-router-dom';
 
 class Posts extends Component {
     state = {
@@ -30,11 +31,13 @@ class Posts extends Component {
     render () {
 
         let posts = this.state.posts.map(post => {
-            return <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}/>
+            return <Link to={'/' + post.id} key={post.id}>
+                        <Post
+                          key={post.id}
+                          title={post.title}
+                          author={post.author}
+                          clicked={() => this.postSelectedHandler(post.id)}/>
+                    </Link>
         });
 
         return (
