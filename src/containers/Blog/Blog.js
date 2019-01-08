774dfeb31c581f52b5ce241f3a8838d26d59dfce
PRITAM <pritam.das@mail.utoronto.ca>
2019-01-08 13:57:41 -0500

    Set up project to enable routing. Made Posts, New Posts and Full Posts as separate containers

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,39 +1,12 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null
-    }
-
-    componentDidMount () {
-        axios.get('/posts')
-          .then(response => {
-                const posts = response.data.slice(0,4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'PD'
-                    }
-                })
-                this.setState({posts: updatedPosts})
-          })
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}/>
-        });
+
 
         return (
             <div className="Blog">
@@ -45,15 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+  
+                <Posts />
             </div>
         );
     }
