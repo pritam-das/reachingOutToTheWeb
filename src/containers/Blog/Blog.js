43dabc9bfee8aab13ebf9c38f58cbd2dbd552391
PRITAM <pritam.das@mail.utoronto.ca>
2019-01-08 14:05:04 -0500

    Added logic to load Posts components upon setting '/' route. Enabled routing in Blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import axios from 'axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -18,8 +19,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-  
-                <Posts />
+                <Route path="/" exact component={Posts}/>
             </div>
         );
     }
